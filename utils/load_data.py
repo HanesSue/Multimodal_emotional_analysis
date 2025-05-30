16e08e7d6e5018ea896a8ba69535fcb19de4fe09
HanesSue <1049705784@qq.com>
2025-05-30 18:52:22 +0800

    更新了dataset

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,8 +1,7 @@
 # -*- coding : utf-8 -*-
-
-from torch.utils.data import Dataset
 import pandas as pd
 import os
+from torch.utils.data import Dataset
 
 
 class SIMSData(Dataset):
@@ -27,27 +26,30 @@ class SIMSData(Dataset):
     @property
     def video_split(self):
         if self._video_split is None:
-            self._video_split = []
+            splits = []
             raw_path = os.path.join(self.root, "Raw")
             for root, _, files in os.walk(raw_path):
-                video_id = root.split("/")[-1]
+                video_id = os.path.basename(root)
                 for file in files:
-                    split_id = file.split(".")[0]
+                    clip_id = file.split(".")[0]
                     split_path = os.path.join(root, file)
-                    self._video_split.append((video_id, split_id, split_path))
+                    splits.append((video_id, clip_id, split_path))
+            self._video_split = pd.DataFrame(splits, columns=["video_id", "clip_id", "split_path"])
         return self._video_split
 
-    def _load_data(self):
-        pass
+    def _get_path(self, video_id, clip_id):
+        return self.video_split.loc[(self.video_split["video_id"] == video_id) & (self.video_split["clip_id"] == clip_id), "split_path"].values[0]
 
     def __len__(self):
-        return 0
+        return len(self.meta)
 
-    def __getitem__(self, index):
-        return index
-
+    def __getitem__(self, idx):
+        video_id, clip_id, text = self.meta.iloc[idx][["video_id", "clip_id", "text"]]
+        split_path = self._get_path(video_id, clip_id)
+        return split_path, text
 
 if __name__ == "__main__":
     test = SIMSData(root="./data/ch-sims2s/ch-simsv2s", mode="test")
+    print(test.video_split.head())
     print(test.meta.head())
-    print(test.video_split[:10])
+    print(test[0])
