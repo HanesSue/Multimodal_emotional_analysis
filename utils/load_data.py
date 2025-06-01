b3633bcd0138e9eed50ef7191bcc159cdf3f22d8
HanesSue <1049705784@qq.com>
2025-06-01 17:07:57 +0800

    更正了Load_Data的路径加载逻辑

--- utils/load_data.py
+++ utils/load_data.py
@@ -10,7 +10,6 @@ class SIMSData(Dataset):
         self.root = kwargs["root"]
         self.mode = kwargs["mode"]
         self._raw_meta = None
-        self._video_split = None
 
     @property
     def meta(self):
@@ -23,22 +22,18 @@ class SIMSData(Dataset):
 
         return self._raw_meta[self._raw_meta["mode"] == self.mode]
 
-    @property
-    def video_split(self):
-        if self._video_split is None:
-            splits = []
-            raw_path = os.path.join(self.root, "Raw")
-            for root, _, files in os.walk(raw_path):
-                video_id = os.path.basename(root)
-                for file in files:
-                    clip_id = file.split(".")[0]
-                    split_path = os.path.join(root, file)
-                    splits.append((video_id, clip_id, split_path))
-            self._video_split = pd.DataFrame(splits, columns=["video_id", "clip_id", "split_path"])
-        return self._video_split
-
     def _get_path(self, video_id, clip_id):
-        return self.video_split.loc[(self.video_split["video_id"] == video_id) & (self.video_split["clip_id"] == clip_id), "split_path"].values[0]
+        return os.path.join(self.root, "Raw", f"{video_id}/{clip_id}.mp4")
+
+    def _get_labels(self, video_id, clip_id):
+        return (
+            self.meta.loc[
+                (self.meta["video_id"] == video_id) & (self.meta["clip_id"] == clip_id),
+                ["label", "label_T", "label_A", "label_V"],
+            ]
+            .values[0]
+            .tolist()
+        )
 
     def __len__(self):
         return len(self.meta)
@@ -46,10 +41,11 @@ class SIMSData(Dataset):
     def __getitem__(self, idx):
         video_id, clip_id, text = self.meta.iloc[idx][["video_id", "clip_id", "text"]]
         split_path = self._get_path(video_id, clip_id)
-        return split_path, text
+        labels = self._get_labels(video_id, clip_id)
+        return split_path, text, labels
+
 
 if __name__ == "__main__":
     test = SIMSData(root="./data/ch-sims2s/ch-simsv2s", mode="test")
-    print(test.video_split.head())
     print(test.meta.head())
     print(test[0])
